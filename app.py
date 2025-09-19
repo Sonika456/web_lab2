3fc11a1c7f22eed6483b4cd8f3868f76c4e114ba
Соня Черевцова <sonikacherevtsova@gmail.com>
2025-09-19 17:56:23 +0700

    Счётчик

--- app.py
+++ app.py
@@ -39,3 +39,16 @@ def image():
         </body>
     </html>
     '''
+count = 0
+@app.route("/counter")
+def counter():
+    global count
+    count += 1
+    return '''
+    <!doctype html>
+    <html>
+        <body>
+            Сколько раз вы сюда захлдили: ''' + str(count) + '''
+        </body>
+    </html>
+    '''
