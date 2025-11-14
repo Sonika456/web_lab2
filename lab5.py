d7666d880b63bb7120382203d5859446930eaa71
Соня Черевцова <sonikacherevtsova@gmail.com>
2025-11-14 19:36:23 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template, request, session
+from flask import Blueprint, url_for, render_template, request, session, redirect
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -66,3 +66,20 @@ def login():
     session['login'] = login
     db_close(conn, cur)
     return render_template('/lab5/success_login.html', login=login)
+
+
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login: 
+        return redirect('/lab5/login')
+    if request.method == 'GET': 
+        return render_template('/lab5/create_article.html')
+    title = request.form.get('title') 
+    article_text = request.form.get('article_text')
+    conn, cur = db_connect()
+    cur.execute ("SELECT * FROM users WHERE login=%s;", (login, )) 
+    user_id = cur.fetchone() ["id"]
+    cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s);", (user_id, title, article_text))
+    db_close(conn, cur) 
+    return redirect('/lab5')
