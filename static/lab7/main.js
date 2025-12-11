5e9a4924d960e358f48692cdc18494098bd1bb54
Соня Черевцова <sonikacherevtsova@gmail.com>
2025-12-11 19:36:37 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -20,6 +20,9 @@ function fillFilmList() {
 
             let editButton = document.createElement('button'); 
             editButton.innerText = 'Редактировать';
+            editButton.onclick = function() {
+                editFilm(i);
+            };
 
             let delButton = document.createElement('button'); 
             delButton.innerText = 'Удалить';
@@ -50,7 +53,6 @@ function deleteFilm(id, title) {
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
 }
-
 function hideModal() {
     document.querySelector('div.modal').style.display = 'none';
 }
@@ -66,6 +68,7 @@ function addFilm() {
     showModal();
 }
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film= {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value, 
@@ -73,16 +76,30 @@ function sendFilm() {
         description: document.getElementById('description').value
     }
 
-    const url='/lab7/rest-api/films/'; 
+    const url=`/lab7/rest-api/films/${id}`; 
     const method = 'POST';
 
     fetch(url, {
-    method: method,
-    headers: {"Content-Type": "application/json"}, 
-    body: JSON.stringify(film)
+        method: method,
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify(film)
     })
     .then(function() { 
         fillFilmList(); 
         hideModal();
+    });
+}
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description; 
+        showModal();
     });
 }
