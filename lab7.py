72fbe8aa3da83af14abdd4d1da612c92851ab5fd
Соня Черевцова <sonikacherevtsova@gmail.com>
2025-12-12 01:54:30 +0700

    дополнительное задание 2

--- lab7.py
+++ lab7.py
@@ -1,78 +1,80 @@
-from flask import Blueprint, url_for, render_template, abort, request
+from flask import Blueprint, url_for, render_template, abort, request, current_app
 from datetime import datetime
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import sqlite3
+from os import path
 
 lab7 = Blueprint('lab7', __name__)
+
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'cherevtsova_knowledge_base',
+            user = 'cherevtsova_knowledge_base',
+            password = '@R1am1584VT%'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    return conn, cur
+
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 @lab7.route('/lab7/')
 def lab77():
     index_url = url_for('index')
     return render_template('/lab7/lab7.html', index_url=index_url)
 
-films = [
-    {
-        "title": "Interstellar",
-        "title_ru": "Интерстеллар ",
-        "year": 2014,
-        "description": "Когда засуха, пыльные бури и вымирание растений приводят человечество к продовольственному кризису, \
-            коллектив исследователей и учёных отправляется сквозь червоточину (которая предположительно соединяет области \
-            пространства-времени через большое расстояние) в путешествие, чтобы превзойти прежние ограничения для космических \
-            путешествий человека и найти планету с подходящими для человечества условиями."
-    },
-    {
-        "title": "The Shawshank Redemption",
-        "title_ru": "Побег из Шоушенка",
-        "year": 1994,
-        "description": "Бухгалтер Энди Дюфрейн обвинён в убийстве собственной жены и её любовника. Оказавшись в тюрьме под \
-            названием Шоушенк, он сталкивается с жестокостью и беззаконием, царящими по обе стороны решётки. Каждый, кто попадает \
-            в эти стены, становится их рабом до конца жизни. Но Энди, обладающий живым умом и доброй душой, находит подход как к \
-            заключённым, так и к охранникам, добиваясь их особого к себе расположения."
-    },
-    {
-        "title": "The Green Mile",
-        "title_ru": "Зеленая миля",
-        "year": 1999,
-        "description": "Пол Эджкомб — начальник блока смертников в тюрьме «Холодная гора», каждый из узников которого однажды \
-            проходит «зеленую милю» по пути к месту казни. Пол повидал много заключённых и надзирателей за время работы. Однако \
-            гигант Джон Коффи, обвинённый в страшном преступлении, стал одним из самых необычных обитателей блока."
-    },
-    {
-        "title": "Gone Girl",
-        "title_ru": "Исчезнувшая ",
-        "year": 2014,
-        "description": "Всё было готово для празднования пятилетия супружеской жизни, когда вдруг необъяснимо пропала виновница \
-            торжества. Остались следы борьбы в доме, кровь, которую явно пытались стереть, и цепочка подсказок в игре «охота за \
-            сокровищами» - жена ежегодно устраивала её для своего обожаемого мужа. И похоже, что эти подсказки дают шанс \
-            пролить свет на судьбу исчезнувшей."
-    },
-    {
-        "title": "Maleficen",
-        "title_ru": "Малефисента",
-        "year": 2014,
-        "description": "Юная волшебница Малефисента вела уединенную жизнь в зачарованном лесу, окруженная сказочными существами, \
-            но однажды все изменилось… В её мир вторглись люди, которые принесли с собой разрушение и хаос, и Малефисенте пришлось \
-                встать на защиту своих подданных, призвав на помощь могущественные тёмные силы."
-    },
-]
 
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
-    return films
+    conn, cur = db_connect()
+    cur.execute("SELECT id, title, title_ru, year, description FROM films ORDER BY id DESC;")
+    films = cur.fetchall()
+    db_close(conn, cur)
+    return [dict(f) for f in films]
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
-    films_count = len(films)
-    if id >= 0 and id < films_count:
-        return films[id]
+    conn, cur = db_connect()
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT id, title, title_ru, year, description FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("SELECT id, title, title_ru, year, description FROM films WHERE id = ?;", (id,))
+        
+    film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if film:
+        return dict(film)
     else:
         abort(404)
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE']) 
 def del_film(id): 
-    films_count = len(films)
-    if id >= 0 and id < films_count:
-        del films[id]
+    conn, cur = db_connect()
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("DELETE FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("DELETE FROM films WHERE id = ?;", (id,))
+        
+    rows_deleted = cur.rowcount
+    db_close(conn, cur)
+    
+    if rows_deleted > 0:
         return '', 204
     else:
         abort(404)
@@ -114,18 +116,48 @@ def validate_film(film_data):
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT']) 
 def put_film(id):
-    films_count = len(films)
-    if id >= 0 and id < films_count:
-        film = request.get_json()
-        validation_errors = validate_film(film)
-        if validation_errors:
-            return validation_errors, 400
-        if not film.get('title') and film.get('title_ru'):
-            film['title'] = film['title_ru']
-        films[id] = film
-        return films[id]
+    film = request.get_json()
+    validation_errors = validate_film(film)
+    if validation_errors:
+        return validation_errors, 400
+
+    conn, cur = db_connect()
+    
+    if not film.get('title') and film.get('title_ru'):
+        film['title'] = film['title_ru']
+
+    data = (
+        film['title_ru'].strip(), 
+        film['title'].strip(), 
+        int(film['year']), 
+        film['description'].strip(),
+        id 
+    )
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(
+            """
+            UPDATE films SET 
+                title_ru = %s, title = %s, year = %s, description = %s
+            WHERE id = %s;
+            """, data
+        )
     else:
+        cur.execute(
+            """
+            UPDATE films SET 
+                title_ru = ?, title = ?, year = ?, description = ?
+            WHERE id = ?;
+            """, data
+        )
+    
+    rows_updated = cur.rowcount
+    db_close(conn, cur)
+    
+    if rows_updated == 0:
         abort(404)
+    film['id'] = id 
+    return film
 
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
@@ -136,8 +168,26 @@ def add_films():
     validation_errors = validate_film(new_film_data)
     if validation_errors:
         return validation_errors, 400
+    conn, cur = db_connect()
     if not new_film_data.get('title') and new_film_data.get('title_ru'):
         new_film_data['title'] = new_film_data['title_ru']
-    films.append(new_film_data)
-    new_film_id = len(films) - 1
-    return {'id': new_film_id}
+    data = (
+        new_film_data['title_ru'].strip(), 
+        new_film_data['title'].strip(), 
+        int(new_film_data['year']), 
+        new_film_data['description'].strip()
+    )
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(
+            """INSERT INTO films (title_ru, title, year, description) VALUES (%s, %s, %s, %s) RETURNING id;""", data
+        )
+        new_id = cur.fetchone()['id']
+    else:
+        cur.execute(
+            """INSERT INTO films (title_ru, title, year, description) VALUES (?, ?, ?, ?);""", data
+        )
+        new_id = cur.lastrowid
+
+    db_close(conn, cur)
+    return {'id': new_id}
