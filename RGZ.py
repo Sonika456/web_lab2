2c2a73fab47d87e62bbfc2944e32e7da6497cb48
Соня Черевцова <sonikacherevtsova@gmail.com>
2026-01-18 14:05:22 +0700

    переделка2

--- RGZ.py
+++ RGZ.py
@@ -22,7 +22,7 @@ def db_connect():
     else:
         # SQLite
         dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "rgz_database.db")
+        db_path = path.join(dir_path, "database.db")
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
